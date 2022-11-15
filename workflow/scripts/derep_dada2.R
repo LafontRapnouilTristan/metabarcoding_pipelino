a101526e554a37f0a5e41ff0201b562993b14387
LafontRapnouilTristan <bananabegood212@gmail.com>
2022-11-15 17:22:53 -0300

    keeping sample wise count after dada2

--- workflow/scripts/derep_dada2.R
+++ workflow/scripts/derep_dada2.R
@@ -4,6 +4,29 @@ sink(log)
 sink(log, type="message")
 
 library(dada2)
-derepFastq(snakemake@input[[1]],
-           n=snakemake@params[[1]],
-           verbose=TRUE)
+
+input <- snakemake@input[[1]]
+uniques <- derepFastq(input,
+                    #  n=snakemake@params[[1]],
+                      verbose=TRUE)
+
+seqtab <- makeSequenceTable(uniques)
+rownames(seqtab) <- gsub(rownames(seqtab),pattern = ".fastq", replacement = "")
+
+ids <- NULL
+ids_per_seq <- list()
+for (i in 1:ncol(seqtab)){
+  ids <- NULL
+  count <- sum(seqtab[,i] )
+  for (j in 1:nrow(seqtab)){
+    if (seqtab[j,i]!=0){
+      ids <- c(ids, paste0(rownames(seqtab)[j],": ",seqtab[j,i],";", collapse = ""))
+    } 
+  } 
+  ids_per_seq[[i]]  <- paste0("seq",i,";count=",count,";",paste0(ids,collapse = ""))
+}
+names(ids_per_seq) <- colnames(seqtab)
+
+uniquesToFasta(getUniques(seqtab),snakemake@output[1],ids=unlist(ids_per_seq))
+
+
