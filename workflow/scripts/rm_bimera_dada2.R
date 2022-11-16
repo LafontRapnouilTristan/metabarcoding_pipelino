8031fbf83f953a10603ff67d8956a4852f7e9e8f
LafontRapnouilTristan <bananabegood212@gmail.com>
2022-11-16 13:33:50 -0300

    link obi and dada

--- workflow/scripts/rm_bimera_dada2.R
+++ workflow/scripts/rm_bimera_dada2.R
@@ -3,18 +3,23 @@ log <- file(snakemake@log[[1]], open="wt")
 sink(log)
 sink(log, type="message")
 
-library(dada2) #1.24.0
-library(ShortRead) #1.54.0
+library(dada2)
+library(stringr)
 
-tab <- ShortRead::readFasta(snakemake@input[[1]])
-seqtab_1 <- makeSequenceTable(tab)
-seqtab_2 <- removeBimeraDenovo(seqtab_1, 
-                               verbose=T,
-                               multithread=snakemake@params[[1]])
-# processes the bimera removal
+cleaned_seq <- getSequences(snakemake@input[[1]])
+tab <- data.frame(seq_id = str_extract(names(cleaned_seq),"seq[0-9]+"),sequence=cleaned_seq, abundance=str_extract(names(cleaned_seq),"(?<= count=)[0-9]"))
 
-uniqueSeqs <- getUniques(seqtab_2)
-uniquesToFasta(uniqueSeqs,
-               paste0(sample.names, ".fasta"))
-               # creates the new file without bimeras
-               
+tabseq<- makeSequenceTable(tab)
+tabseqnochim <- removeBimeraDenovo(tabseq,
+                                   verbose=T,
+                                   multithread=snakemake@params[[1]])
+tabseqnochim <- getUniques(tabseqnochim)
+
+fasta_headers <- NULL
+for (i in 1:length(tabseqnochim)){
+  fasta_headers <- c(fasta_headers,names(cleaned_seq)[which(cleaned_seq==names(tabseqnochim)[i])] )
+} 
+
+uniquesToFasta(unqs=tabseqnochim,
+               fout=snakemake@output[[1]],
+               ids=fasta_headers)
